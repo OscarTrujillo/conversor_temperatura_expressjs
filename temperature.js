065d51699ed58442aa7b0d6b05fda8c79527c250
Óscar Trujillo <oscartrujillo94@gmail.com>
2015-11-05 22:57:47 +0000

    no encuentra el js

--- temperature.js
+++ temperature.js
@@ -1,9 +1,9 @@
 "use strict"; // Use ECMAScript 5 strict mode in browsers that support it
 function Medida(num,cad) {
-  
+
    var numero=num;
    var cadena=cad;
-    
+
   this.getn =function(){
     return numero
   }
@@ -17,20 +17,20 @@ function Temperatura (num, cad) {
 
     var result;
     var number = parseFloat(this.getn());
-    
+
    ///
    this.c2f=function(){
       result = (number * 9/5)+32;
       result = result.toFixed(1)+" Farenheit"
       return result
     }
-   
+
     this.f2c=function(){
       result = (number - 32)*5/9;
       result = result.toFixed(1)+" Celsius"
       return result
     }
-    
+
    ///
 
 }
@@ -38,37 +38,42 @@ function Temperatura (num, cad) {
 
 
 function calculate() {
+var m;
 
-  var temp = original.value;
-  //var regexp = /([-+]?\d+(?:\.\d*)?)\s*([fFcC])/;
-  var regexp = /([+-]?\d+(?:\.\d*)?(?:\s*[e]\d+)?)\s*([fFcC])/;
-  
-  var m = temp.match(regexp);
-  
+	this.inicializa=function(original){
+		var temp = original;
+	  //var regexp = /([-+]?\d+(?:\.\d*)?)\s*([fFcC])/;
+	  var regexp = /([+-]?\d+(?:\.\d*)?(?:\s*[e]\d+)?)\s*([fFcC])/;
+	  m = temp.match(regexp);
+	}
+
+	this.calc=function(){
+
   if (m) {
-    
-    var num = m[1]; 
+
+    var num = m[1];
     var type = m[2];
     var resultado;
-      
+
       Temperatura.prototype = new Medida;
-            
+
       var a = new Temperatura(num,type);
 
-      
+
       ///
           if (type == 'c' || type == 'C') {
               resultado=a.c2f();
           }
           else{
             resultado=a.f2c();
-              
+
           }
-   converted.innerHTML = resultado;
 
   }
   else {
-    converted.innerHTML = "ERROR! Try something like '-4.2C' instead";
+    resultado = "ERROR! Try something like '-4.2C' instead";
   }
+	return resultado;
+}
 
 }
